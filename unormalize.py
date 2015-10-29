7b049ba101ba5f114e3f3434f7e945205c112a03
Eddie Antonio Santos <easantos@ualberta.ca>
2015-10-29 16:15:42 -0600

    Add nfc, nfd, nfkc, nfkd support.

--- unormalize.py
+++ unormalize.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+"""
+unormalize [args] [FILES]
+
+nfc [args] [files]
+nfd [args] [files]
+
+Converts files or standard input to the given format.
+
+Options
+
+    -i EXTENSION -- Modify files inplace, saving back-ups with EXTENSION
+    -f FORMAT -- normalization form; default is NFC.
+
+"""
+
+import os
 import sys
 import unicodedata
 import fileinput
 
-def process(line):
+def process(line, form='NFC'):
     r"""
     >>> from unicodedata import lookup as u
     >>> line = 'e' + u('COMBINING ACUTE ACCENT') + 'tude\n'
@@ -13,9 +29,19 @@ def process(line):
     '\xc3\xa9tude\n'
     """
     line = line.decode('UTF-8')
-    norm = unicodedata.normalize('NFC', line)
+    norm = unicodedata.normalize(form, line)
     return norm.encode('UTF-8')
 
-if __name__ == '__main__':
+def get_form(program_name):
+    name = os.path.basename(program_name)
+    if name in ['nfc', 'nfd', 'nfkc', 'nfkd']:
+        return name.upper()
+    return None
+
+def main(program_name, *arguments):
+    form = get_form(program_name) or 'NFC'
     for line in fileinput.input():
-        sys.stdout.write(process(line))
+        sys.stdout.write(process(line, form))
+
+if __name__ == '__main__':
+    main(*sys.argv)
