8c73fa8facf2e013e602befb45443a1a7bc9724b
Eddie Antonio Santos <easantos@ualberta.ca>
2020-07-17 12:53:51 -0600

    build: update setup.py

--- setup.py
+++ setup.py
@@ -1,45 +1,47 @@
-from setuptools import setup, find_packages
+import codecs
+from setuptools import find_packages, setup  # type: ignore
 
 import unormalize
 
-import codecs
 
 def long_description():
-    with codecs.open('README.rst', encoding='utf8') as f:
+    with codecs.open("README.rst", encoding="utf8") as f:
         return f.read()
 
+
 setup(
-    name='unormalize',
+    name="unormalize",
     version=unormalize.__version__,
-    description='Unicode normalization filters',
+    description="Unicode normalization filters",
     long_description=long_description(),
-    url='https://github.com/eddieantonio/unormalize',
-    download_url='https://github.com/eddieantonio/unormalize',
+    url="https://github.com/eddieantonio/unormalize",
+    download_url="https://github.com/eddieantonio/unormalize",
     author=unormalize.__author__,
-    author_email='easantos@ualberta.ca',
+    author_email="easantos@ualberta.ca",
     license=unormalize.__license__,
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'unormalize = unormalize.__init__:main',
-            'nfc = unormalize.__init__:nfc',
-            'nfd = unormalize.__init__:nfd',
-            'nfkc = unormalize.__init__:nfkc',
-            'nfkd = unormalize.__init__:nfkd',
+        "console_scripts": [
+            "unormalize = unormalize.__init__:main",
+            "nfc = unormalize.__init__:nfc",
+            "nfd = unormalize.__init__:nfd",
+            "nfkc = unormalize.__init__:nfkc",
+            "nfkd = unormalize.__init__:nfkd",
         ],
     },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Environment :: Console',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Text Processing',
-        'Topic :: Utilities'
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Text Processing",
+        "Topic :: Utilities",
     ],
 )
